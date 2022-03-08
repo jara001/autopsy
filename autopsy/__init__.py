7741edc23121285228852a8bf62c745ddf4e3568
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-08 09:07:43 +0100

    Also store the package version inside the imported module

--- autopsy/__init__.py
+++ autopsy/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3.6
 # __init__.py
 """Initialize script for 'autopsy' package."""
+
+# Version of the package
+from .version import __version__
--- setup.py
+++ setup.py
@@ -39,6 +39,11 @@ with open("VERSION", "w") as file:
     file.write(VERSION)
 
 
+# Also store the version to be seen from the code
+with open("./autopsy/version.py", "w") as file:
+    file.write("__version__ = '%s'" % VERSION)
+
+
 setup(
     name = "autopsy",
     version = VERSION,
