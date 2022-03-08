26ef31aeba68879d6bd5145dc12f9e94e91cdf84
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-08 09:37:29 +0100

    Resolve versioning when building inside ROS workspace
    We are just using the version from `package.xml`. It means that
    the Python versioning is more precise.

--- setup.py
+++ setup.py
@@ -5,43 +5,51 @@
 
 import os
 from setuptools import setup, find_packages
-from _version import Version
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-#def read(fname):
-#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+try:
+    from _version import Version
 
-VERSION = str(Version(os.popen("git describe --tags --dirty --always").read()[1:-1]))
+    # Utility function to read the README file.
+    # Used for the long_description.  It's nice, because now 1) we have a top level
+    # README file and 2) it's easier to type in the README file than to put a raw
+    # string in below ...
+    #def read(fname):
+    #    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-if os.path.exists("VERSION"):
-    STORED = open("VERSION", "r").read()
+    VERSION = str(Version(os.popen("git describe --tags --dirty --always").read()[1:-1]))
 
-    if ".dev" in VERSION:
-        _len = len(VERSION[:VERSION.index(".dev")+4])
-    else:
-        _len = len(VERSION)
+    if os.path.exists("VERSION"):
+        STORED = open("VERSION", "r").read()
 
-    if STORED[:_len] == VERSION[:_len] and ".dev" in VERSION:
-        # Obtain dev number
-        VERSION = VERSION + str(int(STORED[_len:]) + 1)
+        if ".dev" in VERSION:
+            _len = len(VERSION[:VERSION.index(".dev")+4])
+        else:
+            _len = len(VERSION)
+
+        if STORED[:_len] == VERSION[:_len] and ".dev" in VERSION:
+            # Obtain dev number
+            VERSION = VERSION + str(int(STORED[_len:]) + 1)
+        else:
+            if ".dev" in VERSION:
+                VERSION = VERSION + "0"
     else:
         if ".dev" in VERSION:
             VERSION = VERSION + "0"
-else:
-    if ".dev" in VERSION:
-        VERSION = VERSION + "0"
 
 
-with open("VERSION", "w") as file:
-    file.write(VERSION)
+    with open("VERSION", "w") as file:
+        file.write(VERSION)
 
 
-# Also store the version to be seen from the code
-with open("./autopsy/version.py", "w") as file:
-    file.write("__version__ = '%s'" % VERSION)
+    # Also store the version to be seen from the code
+    with open("./autopsy/version.py", "w") as file:
+        file.write("__version__ = '%s'" % VERSION)
+except:
+    # When not building the wheel use the package version
+    VERSION=str(os.popen("grep '<version>' package.xml | grep -o '[0-9\.]*'").read()[:-1])
+
+    with open("./autopsy/version.py", "w") as file:
+        file.write("__version__ = '%s'" % VERSION)
 
 
 setup(
